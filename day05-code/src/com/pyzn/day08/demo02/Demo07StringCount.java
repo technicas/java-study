da3b6a52f7356dcb45f030290702d82fbbd7e0fd
pyzn <pyzn@ive.cn>
2020-06-15 11:27:01 +0800

    add StringCount

--- day05-code/src/com/pyzn/day08/demo02/Demo07StringCount.java
+++ day05-code/src/com/pyzn/day08/demo02/Demo07StringCount.java
@@ -2,6 +2,10 @@ package com.pyzn.day08.demo02;
 
 import java.util.Scanner;
 
+/*
+键盘输入一个字符串，并且统计其中各类字符出现的次数
+种类有：大写字母、小写字母、数字、其他
+ */
 public class Demo07StringCount {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -27,5 +31,9 @@ public class Demo07StringCount {
                 countOther++;
             }
         }
+        System.out.println("大写字母出现的次数是：" + countUpper);
+        System.out.println("小写字母出现的次数是：" + countLower);
+        System.out.println("数字出现的次数是：" + countNumber);
+        System.out.println("其它字母出现的次数是：" + countOther);
     }
 }
